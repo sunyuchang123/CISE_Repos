1ff4ea23fccc940111f585e98567b20d40c5f609
sunyuchang123 <vwm9888@autuni.ac.nz>
2024-08-15 15:30:21 +1200

    new one

--- worksheet4/frontend/src/components/nav/NavBar.tsx
+++ worksheet4/frontend/src/components/nav/NavBar.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Nav.module.scss";
 
 type Props = {
-  children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const NavBar = ({ children }: Props) => {
-  return <nav className={styles.navbar}>{children}</nav>;
+  return <nav className={styles.navbar}>{children}</nav>;
 };
 
 export default NavBar;
--- worksheet4/frontend/src/components/nav/NavDropdown.tsx
+++ worksheet4/frontend/src/components/nav/NavDropdown.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Nav.module.scss";
 
 type Props = {
-  children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const NavDropdown = ({ children }: Props) => {
-  return <div className={styles.dropdown_container}>{children}</div>;
+  return <div className={styles.dropdown_container}>{children}</div>;
 };
 
 export default NavDropdown;
